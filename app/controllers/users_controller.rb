6b7ce64dfcd7ab20e7a2188985f4f95cf7cc112d
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-02-06 23:50:23 -0600

    Reads facebook cookie to display user header in badges view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_filter :load_facebook_user
+
   def show
-    @user = User.find(params[:id])
+    @user = User.find_user_with_facebook_graph(@me)
     render 'show'
   end
 end
